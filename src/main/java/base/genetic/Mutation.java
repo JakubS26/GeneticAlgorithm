ff9639dd9f9d469871ce3c5602baeeb25bbc9cc6
JakubS26 <jakub_sokolowski@onet.pl>
2022-06-18 13:23:54 +0200

    Poprawiono mutację

--- src/main/java/base/genetic/Mutation.java
+++ src/main/java/base/genetic/Mutation.java
@@ -4,7 +4,7 @@ import base.*;
 import java.util.Random;
 
 public class Mutation {
-	public static void mutate(Path path, double prob, Matrix m) {
+	public static Path mutate(Path path, double prob, Matrix m) {
 		
 		Random rand = new Random();
 		
@@ -12,21 +12,26 @@ public class Mutation {
 		double geneProb = prob/(double)path.size;
 		int secondGene;
 		
-		//System.out.println(geneProb);
+		Path newPath = new Path(path.size);
 		
 		for(int i=0; i<=path.size-1; i++) {
+			newPath.vertices[i] = path.vertices[i];
+		}
+		
+		for(int i=0; i<=newPath.size-1; i++) {
 			 
 			r = rand.nextDouble();
 			
 			if(r <= geneProb) {
-				secondGene = rand.nextInt(path.size);
-				int temp = path.vertices[i];
-				path.vertices[i] = path.vertices[secondGene];
-				path.vertices[secondGene] = temp;
+				secondGene = rand.nextInt(newPath.size);
+				int temp = newPath.vertices[i];
+				newPath.vertices[i] = newPath.vertices[secondGene];
+				newPath.vertices[secondGene] = temp;
 			}
 			
 		}
 		
-		path.setPathLen(m.objectiveFunction(path));
+		newPath.setPathLen(m.objectiveFunction(newPath));
+		return newPath;
 	}
 }
